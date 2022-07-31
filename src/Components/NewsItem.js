dd1f12b5676c6b56f4d65fc2588cdcbb6ff3a9b4
DeekshaDewangan <104919898+DeekshaDewangan@users.noreply.github.com>
2022-07-31 16:50:59 +0530

    classBased

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,13 +2,21 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl, author, data, source } = this.props;
+    let { title, description, imageUrl, newsUrl, author, data, source } =
+      this.props;
     return (
       <div className="my-3">
         <div className="card">
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>
-                {source}
-              </span>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
           <img
             src={
               imageUrl
@@ -19,13 +27,12 @@ export class NewsItem extends Component {
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">
-              {title}{" "}
-            </h5>
+            <h5 className="card-title">{title} </h5>
             <p className="card-text">{description}</p>
             <p className="card-text">
               <small className="text-muted">
-                Ny {author ? author : "Unknown"} on {new Date(data).toGMTString}
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(data).toGMTString()}
               </small>
             </p>
             <a
